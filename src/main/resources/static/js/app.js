d9756146f1df64cca9151411793cd41e6ed5c000
Opeyemi Raphael-Ogbe <scienceshephard@gmail.com>
2025-01-10 16:44:49 +0100

    Time clock now dynamically fixed

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -36,3 +36,57 @@ function arrowRight() {
 function searchbox(){
     event.preventDefault()
 }
+
+//clock
+const days= ["Sunday", "Monday", "Tuesday", "Wednessday","Thursday", "Friday", "Saturday"]
+
+function clock() {
+
+    const date = new Date()
+    const hours = String(date.getHours()).padStart(2, "0")
+    // const hours = date.getHours() < 10 ? "0" : date.getHours()
+    const minutes= String(date.getMinutes()).padStart(2, "0")
+    const seconds= String(date.getSeconds()).padStart(2, "0")
+    const today = date.toLocaleString('default', {weekday: 'long'})
+    // document.querySelector('.day').textContent = date.toLocaleString('default', {weekday: 'long'})
+    document.querySelector('.time').innerHTML= `
+        <div class="duration">
+            <h1>${today}</h1>
+        </div>
+        <div class="duration"> 
+            <p>Hour</p>
+            <h1>${hours}</h1>
+        </div> 
+        <h1>:<h1>
+        <div class="duration">
+            <p>Minute</p>
+            <h1>${minutes}</h1>
+        </div> 
+        <h1>:<h1>
+        <div class="duration">
+            <p>Seconds</p>  
+            <h1>${seconds}</h1>
+        </div> 
+    ` 
+}
+setInterval(clock, 1000)
+clock();
+
+
+{/* <span>Days</span>
+<h1>03</h1>
+</div>
+<h1>:</h1>
+<div class="duration">
+<span>Hours</span>
+<h1>07</h1>
+</div>
+<h1>:</h1>
+<div class="duration">
+<span>Minutes</span>
+<h1>03</h1>
+</div>
+<h1>:</h1>
+<div class="duration">
+<span>Seconds</span>
+<h1>03</h1> */}
