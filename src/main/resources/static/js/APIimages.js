bad798e9b394c5df34d969306229f8309a5884a3
Opeyemi Raphael-Ogbe <scienceshephard@gmail.com>
2025-04-09 15:20:23 +0100

    Added some changes on how i want to acces each product. When i click a product div on the frontend.

--- src/main/resources/static/js/APIimages.js
+++ src/main/resources/static/js/APIimages.js
@@ -26,15 +26,18 @@ async function loadInitialTechImages() {
         const card = document.createElement("div");
         card.classList.add("card");
         card.innerHTML = `
-            <div class="products-img-card">
+            <div class="products-img-card" onclick="${viewCurrentProduct(image.user.id)}">
                 <img src="${image.urls.regular}" alt="Tech Image" class="fade-in">
             </div>
             <div class="products-card-content">
                 ${image.alt_description || "Tech Image"}
             </div>
         `;
+        // console.log(image);
+        
         imageSlider.appendChild(card);
     });
+    
 }
 
 // Load 8 images when page loads
@@ -51,7 +54,7 @@ async function addNewTechImage() {
     const card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `
-        <div class="products-img-card">
+        <div class="products-img-card" onclick="${viewCurrentProduct(newImage[0].user.id)}">
             <img src="${newImage[0].urls.regular}" alt="New Tech Image" class="fade-in">
         </div>
         <div class="products-card-content">
@@ -62,112 +65,8 @@ async function addNewTechImage() {
     imageSlider.appendChild(card); // Append new image to the end
 }
 
-// Function to fetch Gaming images from the backend
-async function fetchGamingImages(count) {
-    try {
-        const response = await fetch(`/api/images/gadgets/${categories.gaming}?count=${count}`);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
-        const images = await response.json();
-        return images; // Returns an array of images
-    } catch (error) {
-        console.error("Error fetching images:", error);
-    }
-}
-
-async function loadInitialGamesImages() {
-    const images = await fetchGamingImages(8);
+function viewCurrentProduct(url) {
+    console.log(url);
     
-    if (!images) return;
-
-    const imageSlider = document.querySelector(".products-card");
-    imageSlider.innerHTML = ""; // Clear any existing images
-
-    images.forEach((image) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <div class="products-img-card">
-                <img src="${image.urls.regular}" alt="Tech Image" class="fade-in">
-            </div>
-            <div class="products-card-content">
-                ${image.alt_description || "Tech Image"}
-            </div>
-        `;
-        imageSlider.appendChild(card);
-    });
-}
-
-
-// Function to fetch Headphones images from the backend
-async function fetchHeadphonesImages(count) {
-    try {
-        const response = await fetch(`/api/images/${categories.headphones}?count=${count}`);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-        const images = await response.json();
-        return images; // Returns an array of images
-    } catch (error) {
-        console.error("Error fetching images:", error);
-    }
-}
-
-async function loadInitialHeadphonesImages() {
-    const images = await fetchGamingImages(8);
-    
-    if (!images) return;
-
-    const imageSlider = document.querySelector(".products-card");
-    imageSlider.innerHTML = ""; // Clear any existing images
-
-    images.forEach((image) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <div class="products-img-card">
-                <img src="${image.urls.regular}" alt="Tech Image" class="fade-in">
-            </div>
-            <div class="products-card-content">
-                ${image.alt_description || "Tech Image"}
-            </div>
-        `;
-        imageSlider.appendChild(card);
-    });
-}
-
-
-// Function to fetch Smart Watch images from the backend
-async function fetchSmartWatchImages(count) {
-    try {
-        const response = await fetch(`/api/images/${categories.watch}?count=${count}`);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-        const images = await response.json();
-        return images; // Returns an array of images
-    } catch (error) {
-        console.error("Error fetching images:", error);
-    }
-}
-
-async function fetchSmartWatchImages() {
-    const images = await fetchGamingImages(8);
-    
-    if (!images) return;
-
-    const imageSlider = document.querySelector(".products-card");
-    imageSlider.innerHTML = ""; // Clear any existing images
-
-    images.forEach((image) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `
-            <div class="products-img-card">
-                <img src="${image.urls.regular}" alt="Tech Image" class="fade-in">
-            </div>
-            <div class="products-card-content">
-                ${image.alt_description || "Tech Image"}
-            </div>
-        `;
-        imageSlider.appendChild(card);
-    });
 }
